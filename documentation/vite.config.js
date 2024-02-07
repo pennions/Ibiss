da3f370f4981b32eb7e2c2522a74ebd08665d3e2
jelmerveen <jelmerveen@pennions.com>
2024-02-07 21:57:37 +0100

    fix: fix github pages documentation not linking correctly

--- documentation/vite.config.js
+++ documentation/vite.config.js
@@ -1,10 +1,11 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Ibiss/',
   build: {
     outDir: '../docs'
   },
@@ -16,4 +17,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
